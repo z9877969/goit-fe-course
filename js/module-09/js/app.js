7601046fe36423c13f9a52fe1e96bf2971816f56
Andriy Kyrylenko <z9877969@ukr.net>
2019-07-15 17:46:11 +0300

    HW_module-09, 1st-check

---
+++ js/module-09/js/app.js
@@ -0,0 +1,329 @@
+'use strict';
+
+const PRIORITY_TYPES = {
+  LOW: 0,
+  NORMAL: 1,
+  HIGH: 2,
+};
+
+const ICON_TYPES = {
+  EDIT: 'edit',
+  DELETE: 'delete',
+  ARROW_DOWN: 'expand_more',
+  ARROW_UP: 'expand_less',
+};
+
+const NOTE_ACTIONS = {
+  DELETE: 'delete-note',
+  EDIT: 'edit-note',
+  INCREASE_PRIORITY: 'increase-priority',
+  DECREASE_PRIORITY: 'decrease-priority',
+};
+
+const initialNotes = [
+  {
+    id: 'id-1',
+    title: 'JavaScript essentials',
+    body:
+      'Get comfortable with all basic JavaScript concepts: variables, loops, arrays, branching, objects, functions, scopes, prototypes etc',
+    priority: PRIORITY_TYPES.HIGH,
+  },
+  {
+    id: 'id-2',
+    title: 'Refresh HTML and CSS',
+    body:
+      'Need to refresh HTML and CSS concepts, after learning some JavaScript. Maybe get to know CSS Grid and PostCSS, they seem to be trending.',
+    priority: PRIORITY_TYPES.NORMAL,
+  },
+  {
+    id: 'id-3',
+    title: 'Get comfy with Frontend frameworks',
+    body:
+      'First should get some general knowledge about frameworks, then maybe try each one for a week or so. Need to choose between React, Vue and Angular, by reading articles and watching videos.',
+    priority: PRIORITY_TYPES.NORMAL,
+  },
+  {
+    id: 'id-4',
+    title: 'Winter clothes',
+    body:
+      "Winter is coming! Need some really warm clothes: shoes, sweater, hat, jacket, scarf etc. Maybe should get a set of sportwear as well so I'll be able to do some excercises in the park.",
+    priority: PRIORITY_TYPES.LOW,
+  },
+];
+
+class Notepad {
+  constructor (notes = []) {
+  this._notes = notes;
+  }
+
+  get notes () {
+     return this._notes
+  }
+
+  finedNoteById (id) {
+    for (let note of this.notes) {
+      if (id === note.id) return note;
+    };
+  }
+
+  saveNote (note) {
+    return this.notes.push(note);
+  }
+
+  deleteNote (id) {
+    this.notes.splice(this.notes.indexOf(this.finedNoteById(id)), 1);
+  }
+
+  updateNoteContent (id, updatedContent) {
+    return Object.assign(this.finedNoteById(id), updatedContent);
+  }
+
+  updateNotePriority (id, priority) {
+    this.finedNoteById(id).priority = priority;
+    return this.finedNoteById(id).priority;
+  }
+
+  filterNotesByQuery (query) {
+    const resultArr = [];
+    query = query.toLowerCase();
+    for (let note of this.notes) {
+      if (note.body.toLowerCase().includes(query) || note.title.toLowerCase().includes(query)) {
+        resultArr.push(note);
+      };
+    };
+    return resultArr;
+  }
+
+  filterNotesByPriority (priority) {
+    const resultArr = [];
+    for (let note of this.notes) {
+      if (note.priority === priority) {
+        resultArr.push(note);
+      };
+    };
+    return resultArr;
+  }
+}
+
+const notepad = new Notepad(initialNotes);
+
+const noteListLink = document.querySelector('ul.note-list');
+
+const createDomElement = ({tagName, classNameArr, atributesObj, dataAtributesObj, content}) => {
+  const element = document.createElement(tagName);
+  if (classNameArr) {element.classList.add(...classNameArr);}
+  
+  // add data-atribbute
+  if (dataAtributesObj) {
+    Object.entries(dataAtributesObj).forEach(dataAtribute => { 
+      const [dataKey, dataValue] = dataAtribute;
+      element.dataset[dataKey] = dataValue})
+  };
+  // add data-atribbute -END
+  
+  if (atributesObj) {
+    Object.entries(atributesObj).forEach(
+      atribute => {
+        const [atributeKey, atributeValue] = atribute;
+        element.setAttribute(atributeKey, atributeValue);
+      }
+    )
+  };
+  
+  if (content) {element.textContent = content};
+
+  return element;
+};
+
+const createNoteContent = (container, note) => {
+  const dataNoteContent = {
+    tagName: 'div',
+    classNameArr: ['note__content'],
+  };
+  const noteContent = createDomElement(dataNoteContent);
+  container.append(noteContent);
+
+  const dataNoteTitle = {
+    tagName: 'h2',
+    classNameArr: ['note__title'],
+    content: note.title,
+  };
+  const noteTitle = createDomElement(dataNoteTitle);
+  noteContent.append(noteTitle);
+
+  const dataNoteBody = {
+    tagName: 'p',
+    classNameArr: ['note__body'],
+    content: note.body,
+  };
+  const noteBody = createDomElement(dataNoteBody);
+  noteContent.append(noteBody);
+
+  return noteContent;
+}
+
+const createActionButton = (actionLink, iconLink, container) => {
+  const dataActionElement = {
+      tagName: 'button',
+      classNameArr:['action'],
+      dataAtributesObj: {action: actionLink,},
+    };
+    const actionElement = createDomElement(dataActionElement);
+    container.append(actionElement);
+  
+    const dataIconDecrease = {
+      tagName: 'i',
+      classNameArr:['material-icons', 'action__icon'],
+      content: iconLink,
+    };
+    const iconDecrease = createDomElement(dataIconDecrease);
+    actionElement.append(iconDecrease);
+}
+
+const createNoteFooter = (container, note) => {
+  const dataNoteFooter = {
+    tagName: 'footer',
+    classNameArr:['note__footer'],
+  };
+  const noteFooter = createDomElement(dataNoteFooter);
+  container.append(noteFooter);
+
+  // SECTION increase/decrease priority
+  const dataSectionPriority = {
+    tagName: 'section',
+    classNameArr:['note__section'],
+  };
+  const sectionPriority = createDomElement(dataSectionPriority);
+  noteFooter.append(sectionPriority);
+
+  const actionDecrease = createActionButton(
+    NOTE_ACTIONS.INCREASE_PRIORITY,
+    ICON_TYPES.ARROW_DOWN,
+    sectionPriority
+  );
+
+  const actionIncrease = createActionButton(
+    NOTE_ACTIONS.DECREASE_PRIORITY,
+    ICON_TYPES.ARROW_UP,
+    sectionPriority
+  );
+  
+  const dataPriorityStatus = {
+    tagName: 'span',
+    classNameArr:['note__priority'],
+    content: `Priority: ${note.priority}`,
+  };
+  const priorityStatus = createDomElement(dataPriorityStatus);
+  sectionPriority.append(priorityStatus);
+  // SECTION increase/decrease priority -END
+  
+  // SECTION edit/delete note
+  const dataSectionEdit = {
+    tagName: 'section',
+    classNameArr:['note__section'],
+  };
+  const sectionEdit = createDomElement(dataSectionEdit);
+  noteFooter.append(sectionEdit);
+
+  const actionEdit = createActionButton(
+    NOTE_ACTIONS.EDIT,
+    ICON_TYPES.EDIT,
+    sectionEdit
+  )
+
+  const actionDelete = createActionButton(
+    NOTE_ACTIONS.DELETE,
+    ICON_TYPES.DELETE,
+    sectionEdit
+  )
+  // SECTION edit/delete note -END
+
+  return noteFooter;
+}
+
+const createListItem = (note) => {
+  const dataItem = {
+    tagName: 'li',
+    classNameArr: ['note-list__item'],
+    dataAtributesObj: {id: note.id},
+  }
+  const item = createDomElement(dataItem);
+  
+  createNoteContent(item, note);
+  createNoteFooter(item, note);
+  
+  return item;  
+}
+
+const renderNoteList = (listRef, notes) => {
+  const listItemsArr = [];
+  notes.map(el => listItemsArr.push(createListItem(el)));
+  listRef.append(...listItemsArr);
+};
+
+renderNoteList(noteListLink, initialNotes); 
+
+const generateUniqueId = () =>
+  Math.random()
+    .toString(36)
+    .substring(2, 15) +
+  Math.random()
+    .toString(36)
+    .substring(2, 15);
+
+
+const refs = {
+  formNoteEditor: document.querySelector('form.note-editor'),
+  formInput: document.querySelector('input.note-editor__input'),
+  formTextArea: document.querySelector('textarea.note-editor__input'),
+  searchInput: document.querySelector('form.search-form input'),
+}
+
+
+// handlers
+const createNewNote = (event) => {
+  if (refs.formInput.value === '' || refs.formTextArea.value === '') {
+    event.preventDefault();
+    return alert('Необходимо заполнить все поля!')
+  };
+  
+  const newNote = {};  
+  newNote.id = generateUniqueId();
+  newNote.title = refs.formInput.value;
+  newNote.body = refs.formTextArea.value;
+  notepad.saveNote(newNote);
+  newNote.priority = notepad.updateNotePriority(newNote.id, PRIORITY_TYPES.LOW);
+  
+  event.preventDefault();
+  addListItem(noteListLink, createListItem(...notepad.notes.slice(-1)));
+  refs.formNoteEditor.reset();
+
+
+  return newNote;  
+}
+
+const addListItem = (listRef, note) => {
+  listRef.append(note);
+}
+
+const removeListItem = ({target}) => {
+  if(target.closest('button').dataset.action === 'delete-note') {
+    notepad.deleteNote(target.closest('.note-list__item').dataset.id);
+    target.closest('.note-list__item').remove();
+  }
+}
+
+const search = () => {
+  [...noteListLink.children].forEach(item => item.remove());
+  renderNoteList(noteListLink, notepad.filterNotesByQuery(refs.searchInput.value))
+}
+
+
+// Listners
+refs.formNoteEditor.addEventListener('submit', createNewNote);
+
+noteListLink.addEventListener('click', removeListItem);
+
+refs.searchInput.addEventListener('input', search);
+
+
